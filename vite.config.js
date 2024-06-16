2771843c4a50466c300cc0303f7ec12bf2b1a100
dci-student <oliver.sporl@dci-student.org>
2024-06-16 11:30:15 +0200

    vite config edit 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // sicherstellen, dass dies mit deiner Deployment-Konfiguration übereinstimmt
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   },
-})
+  server: {
+    mimeTypes: {
+      '.js': 'application/javascript',
+      '.jsx': 'application/javascript'
+    }
+  }
+});
